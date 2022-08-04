a1337242feb9ec217fb469c47e2a5692d47d055c
Michael LoCascio <michael.s.locascio@gmail.com>
2022-08-03 21:54:32 -0700

    Post and SinglePost components
    Container and Individual post components created along with basic styling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import TopBar from '../src/topbar/TopBar';
+import Posts from '../src/posts/Posts'
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Posts />
     </div>
   );
 }
