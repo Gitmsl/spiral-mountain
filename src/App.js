c76f69ced115f118c810ccdfd6152e3ec6aebeed
Michael LoCascio <michael.s.locascio@gmail.com>
2022-08-04 20:35:19 -0700

    Write page
    Write page component and styling basic setup. Input fields for title and text, button for file upload, button for publishing.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import TopBar from '../src/components/topbar/TopBar';
 import Home from '../src/pages/home/Home'
 import Single from '../src/pages/single/Single'
+import Write from '../src/pages/write/Write'
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      <Single />
+      <Write />
     </div>
   );
 }
