6a80ff9b1494d9520d558eb15c4b01e0e7724c48
Michael LoCascio <michael.s.locascio@gmail.com>
2022-09-12 11:44:30 -0700

    Update + Delete user
    Backend user Update and Delete routes created and functioning

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -43,6 +44,7 @@ mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
 
     app.use("/api/auth", authRoute);
+    app.use("/api/users", userRoute);
 
     app.listen("5000", () => {
         console.log("Backend is running.")
