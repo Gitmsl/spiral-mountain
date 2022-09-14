f8176459c5d3289817fe2928bac2a7880de1aad4
Michael LoCascio <michael.s.locascio@gmail.com>
2022-09-14 16:16:50 -0700

    Post routes
    Create, Update, Delete, and Get posts functional with MongoDB

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -45,6 +46,7 @@ mongoose.connection.once('open', () => {
 
     app.use("/api/auth", authRoute);
     app.use("/api/users", userRoute);
+    app.use("/api/posts", postRoute);
 
     app.listen("5000", () => {
         console.log("Backend is running.")
