681bf7141a921a99719ce7748d62392fba4cb095
Michael LoCascio <michael.s.locascio@gmail.com>
2022-09-29 15:46:31 -0700

    Added key to Posts.jsx
    Key added, linked to unique ID given to each post element by MongoDB. Routes issue to PostsPage.jsx pending fix.

--- client/src/components/singlepost/SinglePost.jsx
+++ client/src/components/singlepost/SinglePost.jsx
@@ -11,7 +11,7 @@ export default function SinglePost({post}) {
                 src={post.photo}
                 alt=""
             />
-        ) ||  (
+        ) || (
             <img 
                 className="postImg"
                 src="https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
