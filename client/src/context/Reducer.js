3e299223533921567d68cf46da44e62adbc2273b
Michael LoCascio <michael.s.locascio@gmail.com>
2023-01-26 12:32:12 -0800

    Context API
    3 files added: Context.js, Actions.js, Reducer.js. Actions.js defines cases for login start, success, and failure; Reducer.js determines the state change according to the case defined in Actions, and Context.js wraps everything together, allowing for value access throughout the app (<App/> in index.js wrapped by ContextProvider)

---
+++ client/src/context/Reducer.js
@@ -0,0 +1,27 @@
+const Reducer = (state, action)=>{
+    switch(action.type){
+        case "login_start":
+            return{
+                user:null,
+                isFetching:true,
+                error:false
+            };
+        case "login_success":
+            return{
+                user:action.payload,
+                isFetching:false,
+                error:false
+            };
+        case "login_failure":
+            return{
+                user:null,
+                isFetching:false,
+                error:true
+            };
+            default:
+                return state;
+
+    }
+};
+
+export default Reducer;
