5a652122203469e6b81b370ed0bd657f5ac31378
Michael LoCascio <michael.s.locascio@gmail.com>
2023-01-27 16:32:27 -0800

    Login.jsx functionality added
    Login page will now allow users to input their credentials and log in. In Context.js, added ability for user credentials to be stored in localstorage as JSON.

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -1,8 +1,8 @@
-import {createContext, useReducer} from "react";
+import {createContext, useEffect, useReducer} from "react";
 import Reducer from "./Reducer";
 
 const initial_state = {
-    user:null,
+    user:JSON.parse(localStorage.getItem("user")) || null,
     isFetching:false,
     error:false,
 };
@@ -11,6 +11,10 @@ export const Context = createContext(initial_state);
 
 export const ContextProvider = ({children}) => {
     const [state, dispatch] = useReducer(Reducer, initial_state);
+
+    useEffect(() => {
+        localStorage.setItem("user", JSON.stringify(state.user));
+    }, [state.user]);
 
     return(
         <Context.Provider 
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,18 +1,57 @@
+import { useContext, useRef } from 'react';
 import { Link } from 'react-router-dom'
+import { Context } from '../../context/Context';
+import axios from 'axios';
 import './login.css'
 
 export default function Login() {
+    
+    const userReference = useRef();
+    const passwordReference = useRef();
+    const { dispatch, isFetching} = useContext(Context);
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        dispatch({type:"login_start"});
+        try{
+            const res = await axios.post("/auth/login", {
+                username: userReference.current.value,
+                password: passwordReference.current.value,
+            })
+            dispatch({type:"login_success", payload:res.data });
+        } catch (err) {
+            dispatch({type:"login_failure"});
+        }
+    };
+
     return (
         <div className='login'>
             <span className='loginTitle'><i className="icon fa-solid fa-puzzle-piece"></i>Login</span>
-            <form className='loginForm'>
+            <form className='loginForm' onSubmit={handleSubmit}>
                 {/* <label>Email</label> */}
-                <input type='text' className='loginInput' placeholder='Email'></input>
+                <input
+                    type='text' 
+                    className='loginInput' 
+                    placeholder='Username'
+                    ref={userReference}
+                />
                 {/* <label>Password</label> */}
-                <input type='password' className='loginInput' placeholder='Password'></input>
+                <input
+                    type='password' 
+                    className='loginInput' 
+                    placeholder='Password'
+                    ref={passwordReference}
+                />
+                <button 
+                    className='loginButton' 
+                    type='submit'
+                    disabled={isFetching}>
+                        <i className="icon fa-solid fa-arrow-right-to-bracket"></i>
+                    Login
+                </button>
             </form>
             <div className='buttonContainer'>
-                <button className='loginButton'><i className="icon fa-solid fa-arrow-right-to-bracket"></i>Login</button>
                 <button className='loginRegisterButton'>
                 <Link className='link' to="/register"><i className="icon fa-solid fa-user-plus"></i>Register new account</Link>
                 </button>
